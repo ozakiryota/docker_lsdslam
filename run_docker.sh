84681d655fc2d221313bf07aab1608edd7513459
ozakiryota <kb22056@gmail.com>
2018-07-07 10:23:56 +0900

    modified the script

--- run_docker.sh
+++ run_docker.sh
@@ -6,9 +6,13 @@ fi
 
 xhost +
 
+#docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg USERNAME=${USER} -t test1:latest .
+
 nvidia-docker run -it --rm \
 	--env="DISPLAY" \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 	--device=/dev/video1:/dev/video0 \
-	test0:latest \
+	test1:latest \
+
+
